2787f5fc2ec0bacfedb7e3f69041b40df9adcc1c
Kutu <code@dobon.dev>
2024-08-21 20:16:05 +0200

    Add license header

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use relm4::{adw::prelude::*, factory::FactoryVecDeque, gtk::prelude::*, prelude::*};
 use relm4::actions::{RelmAction, RelmActionGroup};
 
